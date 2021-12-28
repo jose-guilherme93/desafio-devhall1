ee4ed5316702e0152e42f5690693494756099883
jose-guilherme93 <jose-guilherme93@hotmail.com>
2021-12-28 20:35:30 -0300

    first screen completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import Header from './components/Header/Header'
+import './App.css' 
+import Button from './components/Button/Button'
 
 export default function App() {
     return (
         <>
-        <Header />
+            <Header />
+
+            <main className="bg-image">
+                <section className="main-text">
+                    <p>Desenvolvedores prontos para a ação</p>
+                    <h1>Seu projeto pronto </h1>
+                    <h1> na velocidade da luz</h1>
+
+                    <div className="action-button">
+                    <Button />
+                    </div>
+                </section>
+            </main>
+
         </>
     )
 }
